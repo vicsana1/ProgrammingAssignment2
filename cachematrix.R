00deabdfdbb51a1df64d3ebb1336fc02272082a9
Victor Sanchez <vicsana1@gmail.com>
2014-06-22 17:35:17 +0200

    added comments to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special matrix 
+## that transform a normal matrix into a special list containing methods
+## for acting as a cache for inverse values
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -16,7 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Function that takes a special matrix built using *makeCacheMatrix* and calculates 
+## its inverse if not solved yet. Otherwise, it calculates the inverse and caches the result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
